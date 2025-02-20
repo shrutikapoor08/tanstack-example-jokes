e84e72e59e9ce88bcf2be120b1621210674a5a7d
shrutikapoor08 <shruti.kapoor08@gmail.com>
2025-02-19 21:46:15 -0800

    Adding jokes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,30 +3,45 @@ import * as fs from "node:fs";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/start";
 
-const filePath = "count.txt";
-
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, "utf-8").catch(() => "0")
-  );
+const filePath = "jokes.json";
+interface Joke {
+  question: string;
+  answer: string;
 }
 
-const getCount = createServerFn({
+interface JokesData {
+  jokes: Joke[];
+}
+async function readJokesFromFile(): Promise<JokesData> {
+  try {
+    const data = await fs.promises.readFile(filePath, "utf-8");
+    return JSON.parse(data) as JokesData;
+  } catch {
+    return { jokes: [] };
+  }
+}
+
+const getJokes = createServerFn({
   method: "GET",
 }).handler(() => {
-  return readCount();
+  return readJokesFromFile();
 });
 
-const updateCount = createServerFn({ method: "POST" })
-  .validator((d: number) => d)
-  .handler(async ({ data }) => {
-    const count = await readCount();
-    await fs.promises.writeFile(filePath, `${count + data}`);
+const updatejokes = createServerFn({ method: "POST" })
+  .validator((joke: { question: string; answer: string }) => joke)
+  .handler(async ({ input: { question, answer } }) => {
+    const jokes = await readJokesFromFile();
+    console.log({ joke, question, answer });
+    const joke = { question, answer };
+    await fs.promises.writeFile(
+      filePath,
+      JSON.stringify({ jokes: [...jokes.jokes, joke] })
+    );
   });
 
 export const Route = createFileRoute("/")({
   component: Home,
-  loader: async () => await getCount(),
+  loader: async () => await getJokes(),
 });
 
 function Home() {
@@ -34,15 +49,27 @@ function Home() {
   const state = Route.useLoaderData();
 
   return (
-    <button
-      type="button"
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate();
-        });
-      }}
-    >
-      Add 1 to {state}?
-    </button>
+    <div>
+      <input type="text" placeholder="Enter your joke setup" name="question" />
+      <input
+        type="text"
+        placeholder="Enter your joke punchline"
+        name="answer"
+      />
+      <button
+        type="button"
+        onClick={() => {
+          updatejokes({
+            question: "question", // get from input
+            answer: "answer", // get from input
+          }).then(() => {
+            router.invalidate();
+          });
+        }}
+      >
+        Submit Joke
+      </button>
+      {state?.jokes?.map((joke) => <div>{joke.question}</div>)}
+    </div>
   );
 }
